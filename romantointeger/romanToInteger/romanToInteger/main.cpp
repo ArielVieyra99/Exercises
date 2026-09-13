84a885f8314d0e6c4a2825c9a183590e1c528fe9
Ariel Vieyra <arielvieyra99@gmail.com>
2026-09-13 08:55:54 -0600

    removed twoSumXcode

---
+++ romantointeger/romanToInteger/romanToInteger/main.cpp
@@ -0,0 +1,30 @@
+//
+//  main.cpp
+//  romanToInteger
+//
+//  Created by Ariel Vieyra on 13/09/26.
+//
+//Input: s = "III"
+//Output: 3
+//Explanation: III = 3.
+
+// I V X L C D M
+#include <iostream>
+
+int main() {
+    std::string input = "III";
+    int output = 0;
+    
+    int r = 1;
+    for (int l = 0;l < input.size(); l ++){
+        switch (input[l]) {
+            case 'I':
+                x = x +1;
+                break;
+            case 'X':
+                if()
+        }
+    }
+    
+    
+}
