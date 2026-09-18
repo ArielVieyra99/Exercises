1854dacb8b4e05f90694dd03c076e13d51f17db0
Ariel Vieyra <arielvieyra99@gmail.com>
2026-09-17 21:43:35 -0600

    added move zeros project, started working on it

---
+++ movezeros/main.cpp
@@ -0,0 +1,44 @@
+//
+//  main.cpp
+//  MyTool
+//
+//  Created by Ariel Vieyra on 17/09/26.
+//
+
+//
+//Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+//
+//Note that you must do this in-place without making a copy of the array.
+//
+// 
+//
+//Example 1:
+//
+//Input: nums = [0,1,0,3,12]
+//Output: [1,3,12,0,0]
+//Example 2:
+//
+//Input: nums = [0]
+//Output: [0]
+#include <iostream>
+#include <vector>
+#include <set>
+
+int main () {
+    std::vector <int> nums = {0,1,0,3,12};
+    std::set <int> set;
+    int zero_count = 0;
+    for(int y = 0; y < nums.size(); y++) {
+        if(nums[y] == 0) {
+            zero_count++;
+        }
+        else
+            set.insert(nums[y]);
+    }
+    for(int i = 0; i < zero_count; i++) {
+        nums.push_back(0);
+    }
+    for (int j = 0; j < nums.size(); j++) {
+        std::cout << nums[j];
+    }
+}
